1042df0f5043d4b7c485c6eb5275a26eae4344dc
bigwanggang <bigwanggang@163.com>
2018-04-22 10:14:30 +0800

    compareStr.c

--- c/comparestr.c
+++ c/comparestr.c
@@ -10,17 +10,3 @@ int compare(char s[], char t[]){
 	}
 	return 0;
 }
-#include<stdio.h>
-void main() {
-	char s[20];
-	char t[20];
-	printf("input s:");
-	scanf("%s",s);
-	printf("input t:");
-	scanf("%s",t);
-
-	printf("s: %s",s);
-	printf("t: %s",t);
-
-	printf("s startsWith t? %d\n", compare(s,t));
-}
