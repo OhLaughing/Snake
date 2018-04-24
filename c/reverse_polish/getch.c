28cce14df7e2bf65d57effa1a05b11f16807f5a1
bigwanggang <bigwanggang@163.com>
2018-04-24 17:39:36 +0800

    Create getch.c

---
+++ c/reverse_polish/getch.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<ctype.h>
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp]:getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many charachters\n");
+	else
+		buf[bufp++] = c;
+} 
